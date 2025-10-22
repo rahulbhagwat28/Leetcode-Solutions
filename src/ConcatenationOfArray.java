7225f047254b2f0e3b1fb6f4cfcb1e842ca48492
Rahul Bhagwat <72269385+rahulbhagwat28@users.noreply.github.com>
2025-10-21 20:00:31 -0500

    Leetcode problem concatenation

---
+++ src/ConcatenationOfArray.java
@@ -0,0 +1,18 @@
+
+
+public class ConcatenationOfArray {
+
+    public int[] getConcatenation(int[] nums) {
+        int[] resultarray = new int[nums.length * 2];
+        int count = 0;
+
+        for (int i = 0; i < resultarray.length; i++) {
+            if (i == nums.length) count = 0;
+
+            resultarray[i] = nums[count];
+            count++;
+
+        }
+        return resultarray;
+    }
+}
